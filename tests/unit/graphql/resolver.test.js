02644e68c4de89c01041c32e7e6989b84e7e5ec0
Perfect Makanju <damiperfect@gmail.com>
2019-06-16 15:41:44 +0100

    Fix test

--- tests/unit/graphql/resolver.test.js
+++ tests/unit/graphql/resolver.test.js
@@ -1,18 +1,13 @@
-const makeResolver = require('../../../src/graphql/resolvers');
-
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
+
+const resolver = require('../../../src/graphql/resolvers');
 
 const expect = chai.expect;
 
 chai.use(chaiAsPromised);
 
 describe('GraphQLResolver', () => {
-  let resolver;
-
-  beforeEach(() => {
-    resolver = makeResolver();
-  });
 
   it('should expose Query Resolver', () => {
     expect(resolver).to.have.property('Query');
